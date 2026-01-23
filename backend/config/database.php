17da722049f33c1e397b3d2a650d3203a1234dff
OmoYele Omotanwa <omoyeleomotanwa@OmoYeles-MacBook-Pro.local>
2026-01-23 05:03:04 +0100

    Fix pgsql config to use individual env vars

--- backend/config/database.php
+++ backend/config/database.php
@@ -70,7 +70,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'naijashop'),
